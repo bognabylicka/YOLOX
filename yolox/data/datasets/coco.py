c13b280d697afb03667869fec930bee0df1edd74
bogna.bylicka <bbylicka@bbylicka-u20.sclab.intel.com>
2021-07-28 14:48:51 +0200

    COCO file structure relaxed

--- yolox/data/datasets/coco.py
+++ yolox/data/datasets/coco.py
@@ -40,7 +40,7 @@ class COCODataset(Dataset):
         self.data_dir = data_dir
         self.json_file = json_file
 
-        self.coco = COCO(os.path.join(self.data_dir, "annotations", self.json_file))
+        self.coco = COCO(os.path.join(self.data_dir, self.json_file))
         self.ids = self.coco.getImgIds()
         self.class_ids = sorted(self.coco.getCatIds())
         cats = self.coco.loadCats(self.coco.getCatIds())
