9f2157e02ae67fe4061bf637fd46a8a25b4ac7f4
bogna.bylicka <bbylicka@bbylicka-u20.sclab.intel.com>
2021-08-02 16:31:56 +0200

    adding more variations

--- yolox/exp/build.py
+++ yolox/exp/build.py
@@ -26,7 +26,10 @@ def get_exp_by_name(exp_name):
         "yolox-l": "yolox_l.py",
         "yolox-x": "yolox_x.py",
         "yolox-tiny": "yolox_tiny.py",
-        "yolox-nano": "nano.py",
+        "yolox-640": "yolox_tiny_640.py",
+        "yolox-t-864": "yolox_tiny_864.py",
+	"yolox-nano": "nano.py",
+        "yolox-nano-640": "nano_640.py",
         "yolov3": "yolov3.py",
     }
     filename = filedict[exp_name]
